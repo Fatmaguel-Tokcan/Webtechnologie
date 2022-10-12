00a6c65baf8f2943f6f33f4c213b3ed7154c96d2
edgoad <edgoal@gmail.com>
2022-10-12 14:33:48 +0200

    1.Schritt

---
+++ src/test/java/de/htw/berlin/Webtechnologie/WebtechnologieApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.Webtechnologie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechnologieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
