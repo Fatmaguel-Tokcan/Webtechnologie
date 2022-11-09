7fe84b121cf5b0e64b20dc76e0a0920217367d52
edgoad <edgoal@gmail.com>
2022-11-09 20:19:43 +0100

    REST API Ressource anlegen

---
+++ src/main/java/de/htw/berlin/Webtechnologie/persistence/PersonRepository.java
@@ -0,0 +1,15 @@
+package de.htw.berlin.Webtechnologie.persistence;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
+
+   List<PersonEntity> findAllByFirstName(String firstName);
+
+}
